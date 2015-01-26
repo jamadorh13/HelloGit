024e4eaeb53e7063b28745195b473891fe7d2590
Jeffrey Amador H <jamadorh13@gmail.com>
2015-01-25 20:30:13 -0400

    Segundo commit con todos los archivos agregados ya que el primero no agrego todos

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -10,9 +10,9 @@ namespace HelloGit
     {
         static void Main(string[] args)
         {
-            string a = "GIT";
-            
-            Console.WriteLine("Hello {0}",a);
+            string a = "GIT" +"H";
+
+            Console.WriteLine("Hello {0}", a);
 
             double am = 64.89;
 
@@ -21,81 +21,78 @@ namespace HelloGit
             Console.WriteLine(am);
             for (int i = 1; i <= 12; i++)
             {
-                for (int e = 1; e <=12; e++)
+                for (int e = 1; e <= 12; e++)
                 {
-                   int  aa = i * e;
-                    
+                    int aa = i * e;
 
-                    string ap =Convert.ToString(aa);
-                    if (ap.Length < 1)
-                    {
-                        Console.WriteLine(i + "x" + e + "=" +"0"+ap.ToString());
 
-                    }
-                    else
-                    {
-                        Console.WriteLine(i + "x" + e + "=" + ap);
-                    }
-                    }
+
+
+                    Console.WriteLine(i + "x" + e + "=" + "0" + aa.ToString());
+
                 }
-            Console.ReadLine();
-            Console.Clear();
-            try
-            {
-                
-                double primero = 0;
+
+                }
+                Console.ReadLine();
+                Console.Clear();
+                try
+                {
+
+                    double primero = 0;
                     double segundo = 0;
                     string operador;
                     double result = 0;
-                do
+                    do
+                    {
+
+                        Console.WriteLine("Escriba el primer numero");
+                        primero = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Escriba el segundo numero");
+                        segundo = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Digite para:");
+                        Console.WriteLine("Para sumar: +");
+                        Console.WriteLine("para Restar: -");
+                        Console.WriteLine("Multiplicar: * o x");
+                        Console.WriteLine("Para dividir: / ");
+                        operador = Console.ReadLine();
+
+                        if (operador == "*" || operador == "x" || operador == "X")
+                        {
+                            result = primero * segundo;
+                            Console.WriteLine("El resultado de {0} * {1} = {2}", primero, segundo, result);
+                        }
+                        else if (operador == "+")
+                        {
+                            result = primero + segundo;
+                            Console.WriteLine("El resultado de {0} + {1} = {2}", primero, segundo, result);
+                        }
+                        else if (operador == "-")
+                        {
+                            result = primero - segundo;
+                            Console.WriteLine("El resultado de {0} - {1} = {2}", primero, segundo, result);
+                        }
+                        else if (operador == "/")
+                        {
+                            result = primero / segundo;
+                            Console.WriteLine("El resultado de {0} / {1} = {2}", primero, segundo, result);
+                        }
+                        else { Console.WriteLine("Operador incorrecto Recuerde que es +,-,*,x y /"); }
+                        Console.ReadLine();
+                        Console.Clear();
+
+                    } while (primero != 0 && segundo != 0);
+
+                }
+                catch (Exception ex)
                 {
-                    
-                    Console.WriteLine("Escriba el primer numero");
-                    primero = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Escriba el segundo numero");
-                    segundo = double.Parse(Console.ReadLine());
-                    Console.WriteLine("Digite para:");
-                    Console.WriteLine("Para sumar: +");
-                    Console.WriteLine("para Restar: -");
-                    Console.WriteLine("Multiplicar: * o x");
-                    Console.WriteLine("Para dividir: / ");
-                    operador = Console.ReadLine();
-                    
-                    if (operador == "*" || operador == "x" || operador == "X")
-                    {
-                        result = primero * segundo;
-                        Console.WriteLine("El resultado de {0} * {1} = {2}", primero, segundo, result);
-                    }
-                    else if (operador == "+")
-                    {
-                        result = primero + segundo;
-                        Console.WriteLine("El resultado de {0} + {1} = {2}", primero, segundo, result);
-                    }
-                    else if (operador == "-")
-                    {
-                        result = primero - segundo;
-                        Console.WriteLine("El resultado de {0} - {1} = {2}", primero, segundo, result);
-                    }
-                    else if (operador == "/")
-                    {
-                        result = primero / segundo;
-                        Console.WriteLine("El resultado de {0} / {1} = {2}", primero, segundo, result);
-                    }
-                    else { Console.WriteLine("Operador incorrecto Recuerde que es +,-,*,x y /"); }
+                    string men = ex.Message.ToString();
+                    Console.WriteLine(men);
                     Console.ReadLine();
-                    Console.Clear();
-                    
-                } while ( primero != 0 && segundo !=0);
+                }
 
-            }
-            catch (Exception ex)
-            {
-                string men = ex.Message.ToString();
-                Console.WriteLine(men);
-            }
+
+                //  Console.ReadLine();
             
-
-          //  Console.ReadLine();
         }
     }
 }
--- HelloGit/readme.txt
+++ HelloGit/readme.txt
@@ -1,2 +1,2 @@
 Esta es una app para aprender GIt
-This is an app to learn git
+---This is an app to learn git---
